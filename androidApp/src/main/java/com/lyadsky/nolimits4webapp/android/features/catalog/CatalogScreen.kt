56d0c7ac74a55e7dbbc624c78712dfb5b5d87922
egor.lyadsky <egor.lyadsky@sebbia.org>
2023-04-08 00:56:28 +0300

    переделал навбар

---
+++ androidApp/src/main/java/com/lyadsky/nolimits4webapp/android/features/catalog/CatalogScreen.kt
@@ -0,0 +1,14 @@
+package com.lyadsky.nolimits4webapp.android.features.catalog
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CatalogScreen() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "catalog")
+    }
+}
